a27e1773b92169da8990b88a07e1283dbd0d8a0a
Francesco Paciello <kekkopacio@gmail.com>
2021-02-20 15:23:44 +0100

    Initial Commit

---
+++ instagram/instagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramApp.swift
+//  instagram
+//
+//  Created by Kekko Paciello on 20/02/21.
+//
+
+import SwiftUI
+
+@main
+struct instagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
